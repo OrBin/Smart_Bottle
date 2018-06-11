e042764df8ccb8190a931ed45c24a44eeec68363
Or Bin <orbin50@gmail.com>
2018-06-11 22:57:22 +0300

    Added a photoresistor and updated the Fritzing scheme

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 import utime
-from machine import Pin, PWM, SPI
+from machine import Pin, PWM, SPI, ADC
 from onewire import OneWire
 from ds18x20 import DS18X20
 from nodemcu_gpio_lcd import GpioLcd
 from max7219 import Matrix8x8
 
 
-ds_sensor = DS18X20(OneWire(Pin(12)))  # D6
+wp_temp_sensor = DS18X20(OneWire(Pin(12)))  # D6
 
 # scan for devices on the bus
-rom = ds_sensor.scan()[0]
+rom = wp_temp_sensor.scan()[0]
 
 # Convert temperature to Celsius
-ds_sensor.convert_temp()
+wp_temp_sensor.convert_temp()
 
-print('Temperature (Celsius, waterproof sensor):', ds_sensor.read_temp(rom))
+print('Temperature (Celsius, waterproof sensor):', wp_temp_sensor.read_temp(rom))
+
+photoresistor = ADC(0)  #A0
+print('Photoresistor value (0 to 1024):', photoresistor.read())
